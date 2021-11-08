0d89f29da3289218e0c8a806993edbc37cfeca6f
Pawel Parcz <pawelparcz@gmail.com>
2021-11-08 15:09:13 +0100

    dodanie controllera

---
+++ src/main/java/pl/sdacademy/projektplus/rest/HealthcheckRestController.java
@@ -0,0 +1,15 @@
+package pl.sdacademy.projektplus.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthcheckRestController {
+    @GetMapping
+    public String healthcheck() {
+        return "It's working!";
+    }
+}
+
